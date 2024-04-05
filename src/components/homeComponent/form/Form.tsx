f14e19754610234b4ba9907f2bbc81a02b477dae
Juan Arede <85207414+juanarede@users.noreply.github.com>
2024-04-05 14:55:45 -0300

    cambios

--- src/components/homeComponent/form/Form.tsx
+++ src/components/homeComponent/form/Form.tsx
@@ -34,8 +34,8 @@ export default function form() {
     <section className="bg-gray-900 py-10 md:py-20">
     <div className="container mx-auto px-4">
       <h2 className="text-2xl md:text-4xl font-bold text-white text-center mb-8">Suscríbete a nuestro newsletter</h2>
-      <div className="max-w-xl mx-auto"> {/* Establece el ancho máximo del contenedor del formulario y lo centra */}
-        <form onSubmit={handleSubmit} className="grid grid-cols-1 gap-y-4"> {/* Utiliza grid para apilar los elementos del formulario */}
+      <div className="max-w-xl mx-auto"> 
+        <form onSubmit={handleSubmit} className="grid grid-cols-1 gap-y-4"> 
           <div className="mb-4">
             <label htmlFor="nombre" className="text-white block mb-2">Nombre</label>
             <input
@@ -108,7 +108,7 @@ export default function form() {
               className="w-full px-4 py-2 rounded-lg bg-gray-800 border border-gray-700 text-white focus:outline-none focus:border-blue-500"
             />
           </div>
-          <div className="flex justify-center"> {/* Centra el botón de envío */}
+          <div className="flex justify-center">
             <div className="g-recaptcha" data-sitekey="TU_SITE_KEY"></div>
             <button type="submit" className="bg-pink-700 hover:bg-pink-800 text-white px-6 py-2 rounded-md">Enviar</button>
           </div>
