7b4b7ea448ad88a8fd30efa6bca81f1a2f53827d
Juan Arede <85207414+juanarede@users.noreply.github.com>
2024-04-05 16:32:00 -0300

    cambios

--- src/components/blogComponent/BLogs.tsx
+++ src/components/blogComponent/BLogs.tsx
@@ -1,35 +1,24 @@
-import  { useState, useEffect } from 'react';
-// import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 interface Article {
   title: string;
   description: string;
   url: string;
-  urlToImage: string;
+  image_url: string;
 }
 
 export default function Blogs() {
   const [articles, setArticles] = useState<Article[]>([]);
-  const NEWS_API_KEY = '97f81c421a594572b303d42764d6f3b9';
 
   const getResources = async () => {
-    const res = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${NEWS_API_KEY}`)
+    const res = await fetch(`https://newsdata.io/api/1/news?apikey=pub_414848babc4328aba1c010cf6a67483fc26c2&q=pizza`)
     const data = await res.json();
-    setArticles(data.articles);
+    console.log(data);
+    setArticles(data.results);
   }
   
   useEffect(() => {
     getResources();
-    
-    // const endpoint = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${NEWS_API_KEY}`;
-    // axios.get(endpoint)
-    // .then(response => {
-    //   console.log(response.data.articles);
-    //   setArticles(response.data.articles);
-    // })
-    // .catch(error => {
-    //   console.error('Error al obtener los datos:', error);
-    // });
   }, []); 
 
   return (
@@ -40,8 +29,8 @@ export default function Blogs() {
             {articles?.map((article, index) => (
               <div key={index} className={`card relative bg-white overflow-hidden card${index + 1} md:col-span-2 lg:col-span-1`}>
                 <div className="img-wrapper relative">
-                  {article.urlToImage ? (
-                    <img style={{ height: "20rem" }} className="w-full object-cover object-center rounded-2xl" src={article.urlToImage} alt={`Imagen ${index + 1}`} />
+                  {article.image_url ? (
+                    <img style={{ height: "20rem" }} className="w-full object-cover object-center rounded-2xl" src={article.image_url} alt={`Imagen ${index + 1}`} />
                   ) : (
                     <img style={{ height: "20rem" }} className="w-full object-cover object-center rounded-2xl" src="https://laverdadonline.com/wp-content/uploads/2023/09/no-image.png" alt={`Imagen alternativa ${index + 1}`} />
                   )}
@@ -52,7 +41,7 @@ export default function Blogs() {
                 <div className="text-wrapper p-4">
                   <h3 className="title mb-2" style={{ fontSize: "23px", color: '#333333', fontWeight: "900" }}>{article.title}</h3>
                   <p className="text" style={{ fontSize: "17px", color: '#333333' }}>{article.description}</p>
-                  <a href={article.url} target="_blank" rel="noopener noreferrer" className="bg-pink-700 hover:bg-pink-800 text-white font-bold rounded-full py-2 px-4 mt-4 inline-block">LEER MÁS</a>
+                  {/* <a href={article.url} target="_blank" rel="noopener noreferrer" className="bg-pink-700 hover:bg-pink-800 text-white font-bold rounded-full py-2 px-4 mt-4 inline-block">LEER MÁS</a> */}
                 </div>
               </div>
             ))}
